eb5f5bd9175b342ff0380f71737c5f6cab2cd7fb
Aaron Shim <aaron.k.shim@gmail.com>
2017-03-23 22:51:43 -0700

    Replace C# PCL with F# PCL

---
+++ HelloFSharp/HelloFSharp/App.xaml.fs
@@ -0,0 +1,16 @@
+﻿namespace HelloFSharp
+
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+type App() = 
+    inherit Application()
+
+    do base.LoadFromXaml(typeof<App>) |> ignore
+       base.MainPage <- new HelloFSharp.MainPage()
+    
+    override this.OnStart() = ()  // Handle when your app starts
+    
+    override this.OnSleep() = () // Handle when your app sleeps
+    
+    override this.OnResume() = () // Handle when your app resumes
---
+++ HelloFSharp/HelloFSharp/MainPage.xaml.fs
@@ -0,0 +1,9 @@
+﻿namespace HelloFSharp
+
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+
+type MainPage() = 
+    inherit ContentPage()
+    
+    do base.LoadFromXaml(typeof<MainPage>) |> ignore
